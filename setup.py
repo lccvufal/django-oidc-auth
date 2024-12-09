02c895415ce30089a8f60909446210ee10df39a5
d4n1 <d4n1@d4n1.org>
2024-12-09 16:48:43 -0300

    fix#53: fix auth

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-oidc-auth',
-    version='2.1.0',
+    version='2.1.3',
     description='OpenID Connect client for Django applications',
     long_description='WIP',
     author='Lucas S. Magalhães, Daniel Pimentel, João Paulo de Araújo, Romero Malaquias',
